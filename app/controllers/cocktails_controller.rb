a50fdddb23c6d725c8e7711cc46cfd244e2edfcb
Christine Schorn <cschorn91@gmail.com>
2019-08-23 14:53:27 +1000

    changed cocktail route

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -10,7 +10,7 @@ class CocktailsController < ApplicationController
   def create
     @cocktail = Cocktail.new(cocktail_params)
     if @cocktail.save
-      redirect_to cocktail_path(@cocktail)
+      redirect_to @cocktail
     else
       render :new
     end
